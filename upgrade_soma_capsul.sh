4cee1d74adfd5d29da3fc1ca468210197e946526
harbined <david.harbine@grenoble-inp.org>
2018-11-12 14:55:19 +0100

    Added config.yml and saved_projects.yml

--- upgrade_soma_capsul.sh
+++ upgrade_soma_capsul.sh
@@ -1,11 +1,11 @@
-python3 -m pip uninstall capsul
+python3 -m pip uninstall --yes capsul
 git clone https://github.com/populse/capsul.git /tmp/capsul
 cd /tmp/capsul
 python3 setup.py install --user
 cd /tmp
 rm -rf /tmp/capsul
 
-python3 -m pip uninstall soma-base
+python3 -m pip uninstall --yes soma-base
 git clone https://github.com/populse/soma-base.git /tmp/soma-base
 cd /tmp/soma-base
 python3 setup.py install --user
