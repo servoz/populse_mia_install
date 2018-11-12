cff8bdb10eca74d77c8c46bd43c3c8b5a079246f
harbined <david.harbine@grenoble-inp.org>
2018-11-12 14:39:29 +0100

    Uninstalling capsul and soma-base from pip

--- upgrade_soma_capsul.sh
+++ upgrade_soma_capsul.sh
@@ -1,11 +1,13 @@
+python3 -m pip uninstall capsul
 git clone https://github.com/populse/capsul.git /tmp/capsul
 cd /tmp/capsul
 python3 setup.py install --user
 cd /tmp
-rm -r /tmp/capsul
+rm -rf /tmp/capsul
 
+python3 -m pip uninstall soma-base
 git clone https://github.com/populse/soma-base.git /tmp/soma-base
 cd /tmp/soma-base
 python3 setup.py install --user
 cd /tmp
-rm -r /tmp/capsul
+rm -rf /tmp/soma-base
