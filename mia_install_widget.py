011f91c08d94cd1c01cb501ff2f2f3442d257704
harbined <david.harbine@grenoble-inp.org>
2018-11-12 14:28:51 +0100

    Chmod the bash file to make it executable

--- mia_install_widget.py
+++ mia_install_widget.py
@@ -383,6 +383,7 @@ class MIAInstallWidget(QtWidgets.QWidget):
 
     @staticmethod
     def upgrade_soma_capsul():
+        os.chmod('upgrade_soma_capsul.sh', 777)
         subprocess.call('./upgrade_soma_capsul.sh', shell=True)
 
     @staticmethod
